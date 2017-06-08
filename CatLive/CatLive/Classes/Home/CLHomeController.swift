dd44b15884258329a032e542c6a50e89a3c95a2c
qianjianeng <329426491@qq.com>
2017-06-08 17:20:35 +0800

    update 首页导航条

--- CatLive/CatLive/Classes/Home/CLHomeController.swift
+++ CatLive/CatLive/Classes/Home/CLHomeController.swift
@@ -13,23 +13,52 @@ class CLHomeController: XCViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        buildUI()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    
+    
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+
+// MARK: UI
+extension CLHomeController {
+    
+    fileprivate func buildUI() {
+        buildNavbar()
     }
-    */
+    
+    private func buildNavbar() {
+        
+        //1. 左侧logo
+        let logoImage = UIImage(named: "home_logo")
+        let letfbarItem = UIBarButtonItem(image: logoImage, style: .plain, target: nil, action: nil)
+        navigationItem.leftBarButtonItem = letfbarItem
+        
+        //2. 右侧item
+        let collectImage = UIImage(named: "search_btn_follow")
+        let rightbarItem = UIBarButtonItem(image: collectImage, style: .plain, target: self, action: #selector(followItemClick))
+        navigationItem.rightBarButtonItem = rightbarItem
+        // 事件监听 --> 发送消息 --> 将方法包装SEL  --> 类方法列表 --> IMP
+        
+        // 3.搜索框
+        let searchbarFrame = CGRect(x: 0, y: 0, width: 320, height: 32);
+        let searchBar = UISearchBar(frame: searchbarFrame)
+        searchBar.placeholder = "主播昵称/房间号/链接"
+        navigationItem.titleView = searchBar
+        searchBar.searchBarStyle = .minimal
+        let searchFiled = searchBar.value(forKey: "_searchField") as? UITextField
+        searchFiled?.textColor = UIColor.white
+    }
+}
 
+// MARK: action
+extension CLHomeController {
+    
+    @objc fileprivate func followItemClick () {
+        
+    }
 }
